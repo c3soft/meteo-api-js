bdb66ab5fac9305c29bd6b5b004aa2f3a754603e
Pascal <it.tech@laposte.net>
2019-10-20 16:33:40 +0200

    create fetch for 3 api

--- app.js
+++ app.js
@@ -3,20 +3,39 @@ fetch('https://api.ipify.org?format=json')
 .then(json => {
   const ip = json.ip;
 
-  fetch('http://freegeoip.net/json/' + ip)
-  .then(resultAddress => resultAddress.json())
-  .then(json => {
-    const ville = json.city
-    // console.log(ville)
- 
-
-  fetch('http://api.openweathermap.org/data/2.5/weather?q=lille&appid=ba99412d31367a2f60169d905ef3b403&units=metric')
-  .then(result => result.json())
-  .then(json => {
-    console.log(json)
-  })
- })
+//   fetch('http://freegeoip.net/json/' + ip,{mode: 'no-corps'})
+//   .then(resultAddress => resultAddress.json())
+//   .then(json => {
+//     const ville = json.city
+//    console.log(ville)
+fetch(`http://api.ipstack.com/${ip}?access_key=6bc4ff361803b27886a007bec081ef6e`)
+.then((resultVille) => {
+return resultVille.json() 
 })
+.then((json) =>{
+  const ville = json.city;
+
+
+fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=ba99412d31367a2f60169d905ef3b403&units=metric&lang=fr`)
+  .then((resultWeather) => {
+  return resultWeather.json() 
+})
+.then((resultWeather) =>{
+    console.log(resultWeather)
+  })
+})
+})
+
+//   fetch('http://api.openweathermap.org/data/2.5/weather?q=lille&appid=ba99412d31367a2f60169d905ef3b403&units=metric', {
+//     mode: 'no-cors',
+
+// })
+//   .then(result => result.json())
+//   .then(json => {
+//     console.log(json)
+//   })
+//  })
+// })
 
 
 // function capitalize(str){
@@ -29,3 +48,4 @@ fetch('https://api.ipify.org?format=json')
 //   .then(json => console.log(json.ip))
    
 // }
+
