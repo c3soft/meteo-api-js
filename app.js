a9f6909d80bc2b1672a6715ca1977abb6962dfea
Pascal <fedodev55@gmail.com>
2019-10-21 16:08:17 +0200

    mise en form formulaire

--- app.js
+++ app.js
@@ -1,27 +1,11 @@
-// fetch('https://api.ipify.org?format=json')
-//   .then(resultIp => resultIp.json())
-//   .then(json => {
-//     const ip = json.ip;
-
-// fetch(`http://api.ipstack.com/${ip}?access_key=6bc4ff361803b27886a007bec081ef6e`)
-//   .then((resultVille) => {
-//   return resultVille.json() 
-//   })
-//   .then((json) =>{
-//     const ville = json.city;
-
-
-//     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=ba99412d31367a2f60169d905ef3b403&units=metric&lang=fr`)
-//         .then((resultWeather) => {
-//         return resultWeather.json() 
-//       })
-//       .then((resultWeather) =>{
-//           console.log(resultWeather)
-//      })
-//   })
-// })
-
-
+const weatherIcons ={
+  "Rain": "wi wi-day-rain",
+  "Clouds": "wi wi-day-cloudy",
+  "Clear": "wi wi-day-sunny",
+  "Snow": "wi wi-day-snow",
+  "Mist": "wi wi-day-fog",
+  "Drizzle": "wi wi-day-sleet"
+}
 
 ////////////////////////////////////////////////////////////////
 async function main(){
@@ -50,16 +34,20 @@ function displayWeatherInfos(data){
   const name = data.name;
   const temperature = data.main.temp;
   const descriptions = data.weather[0].description;
+  const conditions = data.weather[0].main;
 
   document.querySelector('#city').textContent = name;
   document.querySelector('#temperature').textContent = temperature;
-  document.querySelector('#conditions').textContent = descriptions;
+  document.querySelector('#conditions').textContent = capitalize(descriptions);
+  document.querySelector('i.wi').className = weatherIcons[conditions];
   
+  document.body.className = conditions.toLowerCase();
+  console.log(conditions.toLowerCase())
 }
 
-//main();
+main();
 ////////////////////////////////////////////////////////////////
 
-// function capitalize(str){
-//     return str[0].toUpperCase() + str.slice(1);
-// };
+function capitalize(str){
+    return str[0].toUpperCase() + str.slice(1);
+};
