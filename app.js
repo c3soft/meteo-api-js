f110211084f156a5c5ee0ffa02cc9d20a2901c72
Pascal <it.tech@laposte.net>
2019-10-26 12:18:40 +0200

    add select city

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const weatherIcons ={
+const weatherIcons = {
   "Rain": "wi wi-day-rain",
   "Clouds": "wi wi-day-cloudy",
   "Clear": "wi wi-day-sunny",
@@ -8,29 +8,29 @@ const weatherIcons ={
 }
 
 ////////////////////////////////////////////////////////////////
-async function main(){
-  // let ville;
+async function main(withIP = true) {
+  let ville;
+  // const changCity = document.getElementById('city')
+  if (withIP) {
+    const ip = await fetch('https://api.ipify.org?format=json')
+      .then(adblockIp => adblockIp.json())
+      .then(json => json.ip)
 
-  // if(withIP){
-      const ip = await fetch('https://api.ipify.org?format=json')
-        .then(adblockIp => adblockIp.json())
-        .then(json => json.ip)
-        
-      const ville = await fetch(`http://api.ipstack.com/${ip}?access_key=6bc4ff361803b27886a007bec081ef6e`)
-        .then(adblockVille => adblockVille.json())
-        .then(json => json.city) 
-  // }else{
-  //     ville = document.querySelector(#city).textContent;
-  // }  
-     const meteo = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Reims&appid=ba99412d31367a2f60169d905ef3b403&units=metric&lang=fr`)
-        .then(adblockWeather => adblockWeather.json())
-        .then(json => json) 
-console.log(meteo);
+    ville = await fetch(`http://api.ipstack.com/${ip}?access_key=6bc4ff361803b27886a007bec081ef6e`)
+      .then(adblockVille => adblockVille.json())
+      .then(json => json.city)
+  } else {
+    ville = document.querySelector('#city').textContent;
+  }
+  const meteo = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=ba99412d31367a2f60169d905ef3b403&units=metric&lang=fr`)
+    .then(adblockWeather => adblockWeather.json())
+    .then(json => json)
+  console.log(meteo);
 
-       displayWeatherInfos(meteo)
+  displayWeatherInfos(meteo)
 }
 
-function displayWeatherInfos(data){
+function displayWeatherInfos(data) {
   const name = data.name;
   const temperature = data.main.temp;
   const tempMax = data.main.temp_max;
@@ -44,17 +44,17 @@ function displayWeatherInfos(data){
   document.querySelector('#tempMin').textContent = Math.round(tempMin);
   document.querySelector('#conditions').textContent = capitalize(descriptions);
   document.querySelector('i.wi').className = weatherIcons[conditions];
-  
+
   document.body.className = conditions.toLowerCase();
   //console.log(conditions.toLowerCase())
 }
 const ville = document.querySelector('#city');
-ville.addEventListener('click',() =>{
-ville.contentEditable = true;
+ville.addEventListener('click', () => {
+  ville.contentEditable = true;
   //  alert('aa')
 });
-ville.addEventListener('keydown',(e) =>{
-  if(e.keyCode === 13){
+ville.addEventListener('keydown', (e) => {
+  if (e.keyCode === 13) {
     e.preventDefault();
     ville.contentEditable = false;
     main(false);
@@ -64,8 +64,8 @@ ville.addEventListener('keydown',(e) =>{
 main();
 ////////////////////////////////////////////////////////////////
 
-function capitalize(str){
-    return str[0].toUpperCase() + str.slice(1);
+function capitalize(str) {
+  return str[0].toUpperCase() + str.slice(1);
 };
 
 ////////////////////////////////////////////////////////////////
